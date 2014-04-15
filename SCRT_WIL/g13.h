51ee7ffe466b6a208f554684e93ad79f9ac4d05d
hbhdytf <hbhdyangtengfei@163.com>
2014-04-15 09:54:31 +0800

    test secshare

---
+++ SCRT_WIL/g13.h
@@ -0,0 +1,14 @@
+//---------------------- G13.H ---------------------------------
+/* G13.H - Header file for package for arithmetic in field G(2^4),
+reduced modulo x^4 + x + 1.
+
+Version of 19 May 1987.
+*/
+
+#ifndef G13.H
+#define G13int
+G13 Add( G13, G13 ) ;
+G13 Mult( G13, G13 ) ;
+G13 Div( G13, G13 ) ;
+#endif
+
---
+++ secsh/runtest.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+./cryptestss.exe testss 3 7 "what the fuck!!! 中华人民共和国"
+
+./cryptest.exe sr ../secret1.txt secret.000 secret.003 secret.004
+
