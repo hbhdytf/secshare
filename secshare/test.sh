6bb41d052690a5df52c138a1afa51d00a8d9e33a
hbhdytf <hbhdyangtengfei@163.com>
2014-05-03 03:22:06 +0800

    test

--- secshare/test.sh
+++ secshare/test.sh
@@ -7,30 +7,37 @@
 #!/bin/bash
 
 cd ~/secshare/secshare/
-trash-put sec.txt.* s1.key s1 s2.key s2
+trash-put sec.txt.* s1.key s1 s2.key s2 s3.key s3
 cd /home/sandy/Fuzzy/
 
 ./genSS 02_03.bmp
 ./recSS 02_03.bmp 02_03.bmp s1.key
-sleep 5s
+sleep 3s
 ./genSS S1001R01.bmp
 ./recSS S1001R01.bmp S1001R01.bmp s2.key
-sleep 5s
+sleep 3s
+./genfinSS f101_1.txt
+./recfinSS f101_1.txt f101_1.txt s3.key
+sleep 3s
 mv ~/Fuzzy/s1.key ~/secshare/secshare/s1.key
 mv ~/Fuzzy/s2.key ~/secshare/secshare/s2.key
+mv ~/Fuzzy/s3.key ~/secshare/secshare/s3.key
 
 cd ~/secshare/secshare/
 ./secshare 2 sec.txt s1 s2 s3
 
-trash-put s1.key
+trash-put *.key
 cd ~/Fuzzy/
 ./recSS 02_03.bmp 02_02.bmp s1.key
 sleep 10s
 ./recSS S1001R01.bmp S1001R03.bmp s2.key
-cd -
 sleep 10s
+./recfinSS f101_1.txt f101_1.txt s3.key
+sleep 10s
+cd -
 mv /home/sandy/Fuzzy/s1.key ./s1.key
 mv /home/sandy/Fuzzy/s2.key ./s2.key
+mv /home/sandy/Fuzzy/s3.key ./s3.key
 ./secshare -2 sec.txt.enc s1 s2 
 
 content=`cat ~/secshare/secshare/sec.txt.enc.dec`
